8beb11bca6a5123e91ae67815df97b3d8bfcf426
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-10-17 20:59:28 -0400

    comito

--- Files/dangeon/script/dangenBer.java
+++ Files/dangeon/script/dangenBer.java
@@ -1,10 +1,20 @@
 public class dangenBer extends Component {
   public SpatialObject[] walls = new SpatialObject[4], doors = new SpatialObject[4];
 
-  public void UpdateRoom(boolean[] status) {
+  private void Obj() {
+    for (int i = 0; i < 4; i++) {
+      walls[i] = myObject.findChildObject("parede" + (i + 1));
+      doors[i] = myObject.findChildObject("door" + (i + 1));
+    }
+  }
+
+  public void UpdateRoom(boolean[] status, String txt) {
+    Obj();
+    Console.log(txt + "room Update");
     for (int i = 0; i < status.length; i++) {
-      doors[i].setEnabled(status[i]);
-      walls[i].setEnabled(!status[i]);
+      Console.log(status[i]);
+      this.doors[i].setEnabled(status[i]);
+      this.walls[i].setEnabled(!status[i]);
     } 
   }
 }
