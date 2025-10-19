114839dfc6bbf6828d1901a2948daacb14ee6c0e
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-10-19 15:24:35 -0400

    comito

--- Files/dangeon/script/dangenBer.java
+++ Files/dangeon/script/dangenBer.java
@@ -2,10 +2,13 @@ public class dangenBer {
   public ObjectFile walls, doors;
 
   public void UpdateRoom(boolean[] status, String txt, SpatialObject objs, VertexFile walls, VertexFile doors) {
+    StringBuilder name = new StringBuilder();
     for (int i = 0; i < status.length; i++) {
+        name.setLength(0);
       VertexFile obj = status[i] ? doors : walls;
       float rot = (i == 0) ? -90 : (i == 1) ? 180 : (i == 2) ? 90 : 0;
-      SpatialObject ob = new SpatialObject("name" + rot);
+      name.append(txt).append(" / ").append(rot);
+      SpatialObject ob = new SpatialObject(name.toString());
       if (i == 3 || i == 2) {
         ob.setTag("wall");
         ob.getPhysics().setPhysicsEntity(new Staticbody());
@@ -23,35 +26,50 @@ public class dangenBer {
       Quaternion rots = new Quaternion();
       rots.setFromEuler(new Vector3(0, rot, 0));
       ob.setRotation(rots);
-      if (i == 3) ob.addComponent(new checkPhysicsL());
-      if (i == 2) ob.addComponent(new checkPhysicsR());
+      if (i == 3) ob.addComponent(new checkPhysicsL(name.toString()));
+      if (i == 2) ob.addComponent(new checkPhysicsR(name.toString()));
     }
   }
 
   public class checkPhysicsL extends Component {
-    float t = 0;
+    private float t = 0;
+    private StringBuilder name = new StringBuilder();
+
+    public checkPhysicsL(String name) {
+      this.name.append(name);
+    } 
 
     void repeat() {
-      if ((t += 0.01f) > .5f) return;
+      if ((t += Time.deltatime()) < 5f) return;
       Vector3 my = myObject.globalPosition;
       Vector3 myp = myObject.position;
       LaserHit hit = new Laser().trace(new Vector3(my.x, my.y + 2, my.z + 5), new Vector3(myp.x + 1, myp.y, myp.z), 8f);
-      if (hit == null || "wall".equals(hit.getObject().tag)) return;
-      Console.log(hit.getObject().name);
+      if (hit == null) return;
+      SpatialObject colobj = hit.getColliderObject();
+      name.append(" / ").append("0");
+      if (colobj == null && name.toString().equals(colobj.name)) return;
+      Console.log(colobj.name);
       myObject.destroy();
-    } 
+    }
   }
 
   public class checkPhysicsR extends Component {
-    float t = 0;
+    private float t = 0;
+    private StringBuilder name = new StringBuilder();
+
+    public checkPhysicsR(String name) {
+      this.name.append(name);
+    }
 
     void repeat() {
-      if ((t += 0.01f) > .5f) return;
+      if ((t += Time.deltatime()) < 5f) return;
       Vector3 my = myObject.globalPosition;
       LaserHit hit = new Laser().trace(new Vector3(my.x + 5, my.y + 2, my.z), myObject.right(), 8f);
-      //if (hit == null || hit.getObject().tag.equals(hit.getObject().tag)) return;
-      if (hit == null || "wall".equals(hit.getObject().tag)) return;
-      Console.log(hit.getObject().getName());
+      if (hit == null) return;
+      SpatialObject colobj = hit.getColliderObject();
+      name.append(" / ").append("90");
+      if (colobj == null && name.toString().equals(colobj.name)) return;
+      Console.log(colobj.name);
       myObject.destroy();
     }
   }
